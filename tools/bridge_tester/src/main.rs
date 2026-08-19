6109179505e4ee1875bb54dbc166d09f1df83c6d
t1k <154753100+t1ktakdev@users.noreply.github.com>
2026-08-19 20:08:28 +0600

    style: rustfmt bridge tester

--- tools/bridge_tester/src/main.rs
+++ tools/bridge_tester/src/main.rs
@@ -1,7 +1,11 @@
 use reprodeck_lib::*;
+
 fn main() {
     match list_sessions() {
-        Ok(v) => println!("list_sessions ok: {}", serde_json::to_string_pretty(&v).unwrap()),
+        Ok(v) => println!(
+            "list_sessions ok: {}",
+            serde_json::to_string_pretty(&v).unwrap()
+        ),
         Err(e) => println!("list_sessions err: {}", e),
     }
 }
